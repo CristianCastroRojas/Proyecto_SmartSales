90cf167f46a9f8a21dea3c16942d8c43ccec3742
I'm Cristian Andres Castro Rojas <147950986+CristianCastroRojas@users.noreply.github.com>
2024-03-19 13:05:32 -0500

    Integracion modulo de generacion de factura

--- src/Vista/Sistema.java
+++ src/Vista/Sistema.java
@@ -14,8 +14,21 @@ import Modelo.ProveedorDao;
 import Modelo.Venta;
 import Modelo.VentaDao;
 import Reportes.Excel;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
 import javax.swing.JOptionPane;
@@ -47,6 +60,7 @@ public class Sistema extends javax.swing.JFrame {
         txtIdpro.setVisible(false);
         AutoCompleteDecorator.decorate(cbxProveedorPro);
         proDao.ConsultarProveedor(cbxProveedorPro);
+        pdf();
     }
 
     // Método para listar clientes en una tabla
@@ -1989,5 +2003,50 @@ public class Sistema extends javax.swing.JFrame {
         txtTelefonoCV.setText("");
         txtDireccionCV.setText("");
         txtRazonCV.setText("");
+    }
+
+    private void pdf() {
+        try {
+            FileOutputStream archivo;
+            File file = new File("src/pdf/venta.pdf");
+            archivo = new FileOutputStream(file);
+            Document doc = new Document();
+            PdfWriter.getInstance(doc, archivo);
+            doc.open();
+            Image img = Image.getInstance("src/Imagenes/Logo.png");
+
+            Paragraph fecha = new Paragraph();
+            Font negrita = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD, BaseColor.BLUE);
+            fecha.add(Chunk.NEWLINE);
+            Date date = new Date();
+            fecha.add("Factura: 1\n"+ "Fecha: " + new SimpleDateFormat("dd-mm-yyyy").format(date) + "\n\n");
+
+            PdfPTable Encabezado = new PdfPTable(4);
+            Encabezado.setWidthPercentage(100);
+            Encabezado.getDefaultCell().setBorder(0);
+            float[] ColumnaEncabezado = new float[]{20f, 30f, 70f, 40f,};
+            Encabezado.setWidths(ColumnaEncabezado);
+            Encabezado.setHorizontalAlignment(Element.ALIGN_LEFT);
+
+            Encabezado.addCell(img);
+
+            String nit = "1090497093";
+            String nom = "SmartSales";
+            String tel = "3232927924";
+            String dir = "Cúcuta";
+            String ra = "SmartSales";
+
+            Encabezado.addCell("");
+            Encabezado.addCell("Nit :" +nit+ "\nNombre: " +nom+ "\nTelefono: " +tel+ "\nDireccion: " +dir+ "\nRazon: "+ra
+            );
+            Encabezado.addCell(fecha);
+            doc.add(Encabezado);
+            
+            
+            doc.close();
+            archivo.close();
+
+        } catch (Exception e) {
+        }
     }
 }
